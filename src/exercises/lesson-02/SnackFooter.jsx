aa39291b172c933b7134374d4706386172ab46fe
Stephanie Jane <stephaniejane@shopforage.com>
2026-07-19 20:15:25 -0400

    created snack app with four component .jsx files, listing and ranking my top five favorite snacks. added a screen capture displaying the output in local repository.

---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,10 @@
+function SnackFooter() {
+  return (
+    <footer>
+      © {new Date().getFullYear()} Willy Wonka. All rights reserved. No cheetos
+      were harmed in the making of this list.
+    </footer>
+  );
+}
+
+export default SnackFooter;
