f8831d266b2b8ea24af030245d8f259c77bc5c20
Stephanie Jane <stephaniejane@shopforage.com>
2026-09-14 13:54:52 -0400

    Lesson 10 Exercise 4: useNavigate + useLocation + 404 Support

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,18 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router';
 
 export default function NotFound() {
-  const pathname = '';
+  const BASE = '/lessons/lesson-10';
+  const { pathname } = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>
+        <strong>{pathname}</strong> is not a known page on this website.
+      </p>
+      <div>
+        <Link to={BASE}>Go Home</Link>
+      </div>
     </section>
   );
 }
