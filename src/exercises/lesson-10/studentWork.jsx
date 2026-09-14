bcf08b3eb2a0afe3229d0e78c31f850fabea4c9f
Stephanie Jane <stephaniejane@shopforage.com>
2026-09-14 00:05:07 -0400

    Lesson 10 Exercise 2: Link vs NavLink + Active Styling

--- src/exercises/lesson-10/studentWork.jsx
+++ src/exercises/lesson-10/studentWork.jsx
@@ -48,11 +48,11 @@ export default function StudentWork() {
 
       <main style={{ padding: 12 }}>
         <Routes>
-          <Route path="/" element={<Home products={products} />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route index element={<Home products={products} />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="products/:id" element={<ProductDetails />} />
           {user.isLoggedIn && (
-            <Route path="/account" element={<Account user={user} />} />
+            <Route path="account" element={<Account user={user} />} />
           )}
           <Route path="*" element={<NotFound />} />
         </Routes>
